699b87e672e45d62c0e0a6e9cb572a6c12d70c55
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2023-11-10 12:26:21 +0100

    Add Create Account Page

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour
+{
+    // Function to change to a new scene
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
